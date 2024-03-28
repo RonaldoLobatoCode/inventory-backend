decbc41eafc53a9da8bc82dfee3ab042112c67c5
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-03-27 18:40:08 -0600

    Servicio de actualizar una categoria por id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,15 @@ public class CategoryRestController {
 	@PostMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
 		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		return response;
+	}
+	
+	/*
+	 * Put categories by id 
+	 */
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@PathVariable Long id, @RequestBody Category category) {
+		ResponseEntity<CategoryResponseRest> response = service.update(category, id);
 		return response;
 	}
 }
