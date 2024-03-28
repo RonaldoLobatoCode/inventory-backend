e941d23c32b263277f338b7402712f461efa11f6
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-03-27 19:11:57 -0600

    Agregando configuración de CORS para el controlador de categorías

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -1,6 +1,7 @@
 package com.company.inventory.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.company.inventory.services.ICategoryService;
 
 import lombok.AllArgsConstructor;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 @AllArgsConstructor
